8477864d18e2c8f1e1b84dc8aed7225ed90335d6
Zacharie-William Gagné <zwgagne@outlook.com>
2021-10-06 17:07:40 -0400

    migrate every able into migrations

---
+++ foodshare-laravel/database/migrations/2021_10_06_210615_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->string('name');
+            $table->string('email');
+            $table->timestamp('email_verified_at')->useCurrent();
+            $table->string('address');
+            $table->string('city');
+            $table->unsignedBigInteger('food_id');
+            $table->string('password');
+            $table->rememberToken()->nullable(false);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
