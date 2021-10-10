e675368411fd9af415a7821572bfee20f87b0b82
Zacharie-William Gagné <zwgagne@outlook.com>
2021-10-10 11:21:17 -0400

    first attempt of seeding

---
+++ foodshare-laravel/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@email.ca',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
